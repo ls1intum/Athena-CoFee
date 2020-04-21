8c36cd137a0b0b1596ae635bae6a6038f1b4473c
Linus Michel <linus.michel@tum.de>
2020-04-21 14:21:33 +0200

    improve script readability; add info-outputs for log

--- .github/workflows/scripts/dockerimage.sh
+++ .github/workflows/scripts/dockerimage.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# Parameter $1 (Component): either "segmentation" or "clustering"
-COMPONENT=$1
+COMPONENT=$1      # Parameter $1 (Component): either "segmentation" or "clustering"
 
-# Address of Docker Registry
-REGISTRY=docker.pkg.github.com
-# Name of the Docker image
-IMAGE=${REGISTRY}/ls1intum/athene/athene-${COMPONENT}
-# Tag image with short commit hash by default
-TAG=$(git rev-parse --short "$GITHUB_SHA")
+echo -e "INFO: Building ${COMPONENT}-component"
+
+REGISTRY=docker.pkg.github.com                          # Address of Docker Registry
+IMAGE=${REGISTRY}/ls1intum/athene/athene-${COMPONENT}   # Name of the Docker image
+TAG=$(git rev-parse --short "$GITHUB_SHA")              # Tag image with short commit hash by default
 
 # Build and Push image
+echo -e "INFO: Build and Push ${IMAGE}:${TAG}"
 cd ${COMPONENT}; docker build . --file Dockerfile --tag ${IMAGE}:${TAG}
 docker push ${IMAGE}:${TAG}
 
-# Tag and Push as branch-name
+# Tag and Push with branch-name
+echo "INFO: Branch-tag and Push image additionally as ${IMAGE}:${GITHUB_REF##*/}"
 docker tag ${IMAGE}:${TAG} ${IMAGE}:${GITHUB_REF##*/}
-echo "Image additionally branch-tagged as ${IMAGE}:${GITHUB_REF##*/}"
 docker push ${IMAGE}:${GITHUB_REF##*/}
 
-# Tag and Push as latest if on master-branch
+# Tag and Push as latest if building on master-branch
 if [ "${GITHUB_REF##*/}" = "master" ]; then
+  echo "INFO: Tag and Push image additionally as ${IMAGE}:latest"
   docker tag ${IMAGE}:${TAG} ${IMAGE}:latest
-  echo "Image additionally tagged as ${IMAGE}:latest"
   docker push ${IMAGE}:latest
 fi
