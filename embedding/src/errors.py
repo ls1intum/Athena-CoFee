abf4935477996c951d66c5df571650288303b3df
Birtan <birtan.gultekin@hotmail.com>
2020-06-21 19:40:58 +0200

    errors from previous pr

--- embedding/src/errors.py
+++ embedding/src/errors.py
@@ -4,3 +4,6 @@ emptyBody = HTTPBadRequest("Empty Request Body", "Cannot process request with em
 requireTwoBlocks = HTTPBadRequest("Need two or more blocks", "Must provide at least two text blocks.")
 requireTextBlocks = HTTPBadRequest("Need text blocks", "Must provide text blocks.")
 requireFeedback = HTTPBadRequest("Need feedback", "Must provide feedback.")
+requireCourseId = HTTPBadRequest("CourseId not provided", "Must provide a course identifier to upload material")
+requireFileData = HTTPBadRequest("File data is not provided", "Must provide file data to upload material")
+requireFileName = HTTPBadRequest("File name is not provided", "Must provide file name to upload material")
