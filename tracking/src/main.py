3c2376664d37b19c19cc430a750abdd4dbab79c7
Jonas Petry <ga27der@mytum.de>
2020-06-30 12:23:22 +0200

    Add jwt token validation

--- tracking/src/main.py
+++ tracking/src/main.py
@@ -1,7 +1,10 @@
 import json
+import base64
 
 from fastapi import FastAPI, Request
+from jwt import decode
 from .database.Connection import Connection
+from os import environ
 
 app = FastAPI()
 
@@ -14,6 +17,11 @@ async def root():
 @app.post("/tracking/feedback/{feedback_id}")
 async def testPost(feedback_id: int, request: Request):
     feedback = await request.json()
+    jwt_token = request.headers.get('x-athene-tracking-authorization')
+    secret_base64 = environ['JWT_SECRET_BASE64']
+    encoded_jwt_token = decode(jwt_token, base64.b64decode(secret_base64), verify=True, algorithms=['HS256'])
+    if (encoded_jwt_token.get('result_id')) != feedback.get('results')[0].get('id'):
+        return {"message": "You are not authorized!"}
     conn = Connection()
     try:
         conn.insert_document('feedback', feedback)
