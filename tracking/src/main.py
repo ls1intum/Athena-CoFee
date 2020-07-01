a6df10a4807e095c26786bcaaa836a5c0e5ff617
Jonas Petry <ga27der@mytum.de>
2020-07-01 10:32:36 +0200

    Add try-catch to jwt decoding

--- tracking/src/main.py
+++ tracking/src/main.py
@@ -2,7 +2,7 @@ import json
 import base64
 
 from fastapi import FastAPI, Request
-from jwt import decode
+from jwt import decode, exceptions
 from .database.Connection import Connection
 from os import environ
 
@@ -14,17 +14,22 @@ async def root():
     return {"message": "Hello World!"}
 
 
-@app.post("/tracking/feedback/{feedback_id}")
-async def testPost(feedback_id: int, request: Request):
+@app.post("/tracking/result/{result_id}")
+async def testPost(result_id: int, request: Request):
     feedback = await request.json()
     jwt_token = request.headers.get('x-athene-tracking-authorization')
     secret_base64 = environ['JWT_SECRET_BASE64']
-    encoded_jwt_token = decode(jwt_token, base64.b64decode(secret_base64), verify=True, algorithms=['HS256'])
-    if (encoded_jwt_token.get('result_id')) != feedback.get('results')[0].get('id'):
-        return {"message": "You are not authorized!"}
-    conn = Connection()
     try:
+        encoded_jwt_token = decode(jwt_token, base64.b64decode(secret_base64), verify=True, algorithms=['HS256'])
+        if (encoded_jwt_token.get('result_id')) != feedback.get('results')[0].get('id'):
+            return {"message": "Please don't spam manually!"}
+    except exceptions as e:
+        print(e)
+        return {"message": "Your token is not valid!"}
+    try:
+        conn = Connection()
         conn.insert_document('feedback', feedback)
     except Exception as e:
+        print(e)
         return {"message": "Saving in the database did not work!"}
-    return {"id": feedback_id}
+    return {"id": result_id}
