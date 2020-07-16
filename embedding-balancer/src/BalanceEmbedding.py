5680543cd21979e6f158fbc9576dcf9c89736a45
Linus Michel <linus.michel@tum.de>
2020-07-16 13:02:01 +0200

    Implement threading

--- embedding-balancer/src/BalanceEmbedding.py
+++ embedding-balancer/src/BalanceEmbedding.py
@@ -6,9 +6,9 @@ from typing import List
 from falcon import Request, Response, HTTP_200
 from numpy import ndarray
 import requests
+from threading import Thread
 from .entities import Sentence, TextBlock, ElmoVector, Embedding
 from .errors import emptyBody, requireTwoBlocks
-
 
 class BalanceEmbedding:
     __logger = getLogger(__name__)
@@ -18,10 +18,39 @@ class BalanceEmbedding:
         if isinstance(o, ndarray): return o.tolist()
         raise TypeError
 
+    # Creates chunks of blocks
+    def createChunks(self, doc):
+        # TODO: Dynamic chunking dependent on target node settings
+        # TODO: Read chunkSize out of config
+        # TODO: Balancing logic
+        # TODO: Handle single block in a chunk (minimum of 2 required)
+        blockchunks = []
+        chunkSize = 2
+        blockcount = len(doc['blocks'])
+        self.__logger.info("Chunk request of {} total blocks in chunks of size {} ...".format(blockcount, chunkSize))
+        # Create equally sized chunks
+        for i in range(0, len(doc['blocks']), chunkSize):
+            self.__logger.info("Adding chunk for blocks {}-{}".format(i+1, min(i + chunkSize, blockcount)))
+            blockchunk = doc['blocks'][i:min(i + chunkSize, blockcount)]
+            # Pass through courseId if provided
+            if "courseId" not in doc:
+                self.__logger.info("No courseId provided in the request")
+                request_chunk = {
+                    "blocks": blockchunk
+                }
+            else:
+                request_chunk = {
+                    "courseId": doc["courseId"],
+                    "blocks": blockchunk
+                }
+            blockchunks.append(request_chunk)
+        return blockchunks
+
     def on_post(self, req: Request, resp: Response) -> None:
         self.__logger.debug("-" * 80)
-        self.__logger.info("Start processing Embedding Request:")
+        self.__logger.info("Incoming Embedding Request")
 
+        # Error handling for wrong requests
         if req.content_length == 0:
             self.__logger.error("{} ({})".format(emptyBody.title, emptyBody.description))
             raise emptyBody
@@ -31,40 +60,33 @@ class BalanceEmbedding:
             self.__logger.error("{} ({})".format(requireTwoBlocks.title, requireTwoBlocks.description))
             raise requireTwoBlocks
 
-        # Declare output variable
-        output = {"embeddings": []}
+        # Computing preparation
+        output = {"embeddings": []}                 # Declare output variable
+        starttime = datetime.now()                  # Start timer
+        computing_result = {}                       # Declare list for individual results
+        thread_list = list()                        # Declare list for started threads
+        request_chunks = self.createChunks(doc)     # Split request into chunks
 
-        # Split into chunks and start processing
-        chunkSize = 50
-        starttime = datetime.now()
-        blockcount = len(doc['blocks'])
-        self.__logger.info("Computing embeddings of {} blocks with a chunk size of {}".format(blockcount, chunkSize))
-        # TODO: Dynamic chunking dependent on target node settings
-        # TODO: Read chunkSize out of config
-        # TODO: Balancing logic
-        for i in range(0, len(doc['blocks']), chunkSize):
-            # TODO: Threaded approach to process blockchunks in parallel
-            self.__logger.info("Processing blocks {} to {}".format(i, min(i + chunkSize, blockcount)))
-            blockchunk = doc['blocks'][i:min(i + chunkSize, blockcount)]
+        # Function to process request chunk
+        def threaded_request(thread_id, url, request):
+            self.__logger.info("Thread {} started processing ...".format(thread_id))
+            # TODO: Variable Timeout
+            computing_result[thread_id] = requests.post(url, data=json.dumps(request), timeout=60)
+            self.__logger.info("Thread {} finished processing.".format(thread_id))
 
-            # Pass through courseId if provided
-            if "courseId" not in doc:
-                self.__logger.info("No courseId provided in the request")
-                request = {
-                    "blocks": blockchunk
-                }
-            else:
-                request = {
-                    "courseId": doc["courseId"],
-                    "blocks": blockchunk
-                }
-
+        # Process chunks concurrently
+        self.__logger.info("Start load balanced computation ...")
+        for i, request in enumerate(request_chunks):
             # TODO: Distinguish between REST-call to processing-node or file-creation for GPU-node
-            # TODO: Timeout
-            response = requests.post('http://localhost:8001/embed', data=json.dumps(request), timeout=60)
+            request_url = 'https://athene01.ase.in.tum.de/embed'
+            t = Thread(target=threaded_request, args=(i, request_url, request,))
+            t.start()
+            thread_list.append(t)
 
-            # Append processed embeddings to output variable
-            output['embeddings'].extend(response.json()['embeddings'])
+        # Merge all computed embeddings together
+        for i, thread in enumerate(thread_list):
+            thread.join()
+            output['embeddings'].extend(computing_result[i].json()['embeddings'])
 
         endtime = datetime.now()
         processingtime = endtime - starttime
