2636f16b59a896e25856da133ae6472fca980cf9
Linus Michel <linus.michel@tum.de>
2020-07-20 11:30:19 +0200

    - Introduce variable for chunkSize of compute node - Split request according to compute power of compute nodes - Improve error handling

--- embedding-balancer/src/BalanceEmbedding.py
+++ embedding-balancer/src/BalanceEmbedding.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from logging import getLogger
+import math
 import os.path
 from typing import List
 from falcon import Request, Response, HTTP_200, HTTP_500
@@ -32,11 +33,11 @@ class BalanceEmbedding:
         # Parse config
         compute_nodes = list()
         for node in config['compute_nodes']:
-            if 'name' not in node or 'url' not in node or 'compute_power' not in node or 'communication_cost' not in node:
-                self.__logger.warning("Skipping Compute Node definition. Not all required variables set")
+            if 'name' not in node or 'url' not in node or 'chunk_size' not in node or 'compute_power' not in node or 'communication_cost' not in node:
+                self.__logger.warning("Skipping Compute Node definition. Not all required variables set: " + str(node))
                 continue
             try:
-                new_node = ComputeNode(name=str(node['name']), url=str(node['url']),
+                new_node = ComputeNode(name=str(node['name']), url=str(node['url']), chunk_size=int(node['chunk_size']),
                                        compute_power=int(node['compute_power']),
                                        communication_cost=int(node['communication_cost']))
                 compute_nodes.append(new_node)
@@ -46,33 +47,46 @@ class BalanceEmbedding:
 
         return compute_nodes
 
+    # Returns reduced doc and part of doc with length <size>
+    def splitBlocks(self, doc, size):
+        blockchunk = doc['blocks'][0:min(len(doc['blocks']), size)]
+        rest = doc['blocks'][min(len(doc['blocks']), size):]
+        # Pass through courseId if provided
+        if "courseId" not in doc:
+            request_chunk = {
+                "blocks": blockchunk
+            }
+            rest = {
+                "blocks": rest
+            }
+        else:
+            request_chunk = {
+                "courseId": doc["courseId"],
+                "blocks": blockchunk
+            }
+            rest = {
+                "courseId": doc["courseId"],
+                "blocks": rest
+            }
+        return request_chunk, rest
+
     # Creates chunks of blocks
     def createChunks(self, doc, compute_nodes):
-        # TODO: Dynamic chunking dependent on target node settings
-        # TODO: Calculate chunkSize for processing nodes
-        # TODO: Balancing logic (communication cost)
-        # TODO: Handle single block in last chunk (minimum of 2 required) -> Include in previous chunk
-        blockchunks = []
-        chunkSize = 50
-        blockcount = len(doc['blocks'])
-        self.__logger.info("Chunk request of {} total blocks in chunks of size {} ...".format(blockcount, chunkSize))
-        # Create equally sized chunks
-        for i in range(0, len(doc['blocks']), chunkSize):
-            self.__logger.info("Adding chunk for blocks {}-{}".format(i+1, min(i + chunkSize, blockcount)))
-            blockchunk = doc['blocks'][i:min(i + chunkSize, blockcount)]
-            # Pass through courseId if provided
-            if "courseId" not in doc:
-                self.__logger.info("No courseId provided in the request")
-                request_chunk = {
-                    "blocks": blockchunk
-                }
+        total_compute_power = 0
+        for node in compute_nodes:
+            total_compute_power += node.compute_power
+
+        rest = doc
+        for node in compute_nodes:
+            node.chunk_quantity = math.ceil(len(doc['blocks']) / total_compute_power * node.compute_power)
+            # Minimum of 2 blocks is required to compute embeddings - prevent this case
+            if len(rest['blocks']) == (node.chunk_quantity + 1):
+                node.blocks, rest = self.splitBlocks(rest, node.chunk_quantity + 1)
             else:
-                request_chunk = {
-                    "courseId": doc["courseId"],
-                    "blocks": blockchunk
-                }
-            blockchunks.append(request_chunk)
-        return blockchunks
+                node.blocks, rest = self.splitBlocks(rest, node.chunk_quantity)
+
+            self.__logger.info("Node {} will process {} blocks".format(node.name, len(node.blocks['blocks'])))
+        # TODO: Balancing logic (communication cost)
 
     def on_post(self, req: Request, resp: Response) -> None:
         self.__logger.debug("-" * 80)
@@ -88,6 +102,9 @@ class BalanceEmbedding:
             self.__logger.error("{} ({})".format(requireTwoBlocks.title, requireTwoBlocks.description))
             raise requireTwoBlocks
 
+        if "courseId" not in doc:
+            self.__logger.info("No courseId provided in the request")
+
         # Computing preparation
         output = {"embeddings": []}                 # Declare output variable
         starttime = datetime.now()                  # Start timer
@@ -101,30 +118,35 @@ class BalanceEmbedding:
             resp.body = "Error parsing compute node config"
             return
 
-        request_chunks = self.createChunks(doc, compute_nodes)  # Split request into chunks
+        #request_chunks = self.createChunks(doc, compute_nodes)  # Split request into chunks
+        self.createChunks(doc, compute_nodes)  # Split request into chunks
 
         # Function to process request chunk
-        def threaded_request(thread_id, url, request, timeout):
-            self.__logger.info("Thread {} started processing ...".format(thread_id))
+        def threaded_request(thread_id, thread_name, url, request, timeout):
+            self.__logger.info("Thread {} ({}) started processing ...".format(thread_id, thread_name))
+            # TODO: Chunk according to compute node's chunkSize
             computing_result[thread_id] = requests.post(url, data=json.dumps(request), timeout=timeout)
-            self.__logger.info("Thread {} finished processing.".format(thread_id))
+            self.__logger.info("Thread {} ({}) finished processing.".format(thread_id, thread_name))
 
         # Process chunks concurrently
         self.__logger.info("Start load balanced computation ...")
-        for i, request in enumerate(request_chunks):
+        for i, node in enumerate(compute_nodes):
             # TODO: Distinguish between REST-call to processing-node or file-creation for GPU-node
-            # TODO: Variable url
-            request_url = 'https://athene01.ase.in.tum.de/embed'
             # TODO: Variable Timeout and error handling for timeout
             timeout = 60
-            t = Thread(target=threaded_request, args=(i, request_url, request, timeout))
-            t.start()
-            thread_list.append(t)
+            if len(node.blocks['blocks']) != 0:
+                t = Thread(target=threaded_request, args=(i, node.name, node.url, node.blocks, timeout))
+                t.start()
+                thread_list.append(t)
 
         # Merge all computed embeddings together
         for i, thread in enumerate(thread_list):
             thread.join()
-            output['embeddings'].extend(computing_result[i].json()['embeddings'])
+            if 'embeddings' in computing_result[i].json():
+                output['embeddings'].extend(computing_result[i].json()['embeddings'])
+            else:
+                # TODO: Resend blocks to another compute node if processing failed
+                self.__logger.error("Error while merging computed embeddings of thread {}: ".format(i))
 
         endtime = datetime.now()                    # Stop timer
         processingtime = endtime - starttime        # Calculate processing time
--- embedding-balancer/src/entities.py
+++ embedding-balancer/src/entities.py
@@ -19,14 +19,18 @@ class Embedding:
 class ComputeNode:
     name: str
     url: str
+    chunk_size: int
+    chunk_quantity: int
+    blocks: []
     compute_power: float
     communication_cost: float
 
-    def __init__(self, name: str, url: str, compute_power: float, communication_cost: float):
+    def __init__(self, name: str, url: str, chunk_size: int, compute_power: float, communication_cost: float):
         self.name = name
         self.url = url
         self.compute_power = compute_power
+        self.chunk_size = chunk_size
         self.communication_cost = communication_cost
 
     def __str__(self):
-        return "Name: " + self.name + ", URL: " + self.url + ", Compute Power: " + str(self.compute_power) + ", Communication Cost: " + str(self.communication_cost)
+        return "Name: " + self.name + ", URL: " + self.url + ", Chunk Size: " + str(self.chunk_size) + ", Compute Power: " + str(self.compute_power) + ", Communication Cost: " + str(self.communication_cost)
