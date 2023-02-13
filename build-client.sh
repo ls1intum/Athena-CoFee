6018826a4427a73efdc7a9b8f3876b1a92347296
Paul Schwind <git@pschwind.de>
2023-02-13 22:36:50 +0100

    migrate protobuf generation to makefiles as well

--- build-client.sh
+++
@@ -1,3 +0,0 @@
-cd protobuf || exit
-protoc --java_out ../client/src/main/java/ clustering.proto # you need to have protobuf installed
-cd ..
--- build-python-protobuf.sh
+++
@@ -1,3 +0,0 @@
-cd protobuf || exit
-protoc --python_out ../load-balancer/src/ clustering.proto # you need to have protobuf installed
-cd ..
