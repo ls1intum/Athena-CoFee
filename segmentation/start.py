f5b008579fce970e6d0705e6088422791d6a4bc8
Paul Schwind <git@pschwind.de>
2023-02-13 17:30:50 +0100

    only reload uvicorn on src change for much better performance

--- segmentation/start.py
+++ segmentation/start.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def start():
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8001, reload=True, reload_dirs=["src"])
 
 
 if __name__ == "__main__":
