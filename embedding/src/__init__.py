3a1203517556d23e579bdeedd592b1354b391808
Paul Schwind <git@pschwind.de>
2023-02-17 23:45:38 +0100

    another attempt at making codacy happy #2

--- embedding/src/__init__.py
+++ embedding/src/__init__.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 import spacy
 # Download the model so that importing it will work
 # see https://stackoverflow.com/a/47297686/4306257
@@ -6,3 +5,4 @@ spacy.cli.download("en_core_web_sm")
 
 # Patch import
 from .patch.patch_spacy_tags import TAG_MAP
+__all__ = ["TAG_MAP"]  # make the linter happy (unused import), this is a patch because of framework conflicts
