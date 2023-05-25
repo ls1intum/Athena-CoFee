a98591b17536c52a239406700d14698956f7de6e
Jan Philip Bernius <janphilip@bernius.net>
2023-05-25 19:03:46 +0200

    Athena Rename and CIT migration

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     signing
 }
 
-group = "de.tum.in.ase.athene"
-version = "0.0.2"
+group = "de.tum.cit.ase"
+version = "1.0.0"
 
 tasks.register("version") {
     println("$version")
@@ -35,13 +35,13 @@ publishing {
             from(components["java"])
 
             pom {
-                name.set("Athene Client")
-                url.set("https://github.com/ls1intum/Athene")
+                name.set("Athena Client")
+                url.set("https://github.com/ls1intum/Athena")
                 description.set("A system to support (semi-)automated assessment of textual exercises.")
                 licenses {
                     license {
                         name.set("MIT License")
-                        url.set("https://github.com/ls1intum/Athene/blob/master/LICENSE.md'")
+                        url.set("https://github.com/ls1intum/Athena/blob/master/LICENSE.md'")
                     }
                 }
                 developers {
@@ -49,24 +49,24 @@ publishing {
                         id.set("jpbernius")
                         name.set("Jan Philip Bernius")
                         email.set("janphilip.bernius@tum.de")
-                        url.set("https://ase.in.tum.de/bernius")
+                        url.set("https://ase.cit.tum.de/bernius")
                         organization.set("Technical University of Munich, Applied Software Engineering")
-                        organizationUrl.set("https://ase.in.tum.de")
+                        organizationUrl.set("https://ase.cit.tum.de")
 
                     }
                 }
                 scm {
-                    connection.set("scm:git:https://github.com/ls1intum/Athene.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:ls1intum/Athene.git'")
-                    url.set("https://github.com/ls1intum/Athene")
+                    connection.set("scm:git:https://github.com/ls1intum/Athena.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:ls1intum/Athena.git'")
+                    url.set("https://github.com/ls1intum/Athena")
                 }
             }
         }
     }
     repositories {
         maven {
-            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
             url = if (isRelease) releasesRepoUrl else snapshotsRepoUrl
             credentials {
                 username = System.getenv("OSSRH_USERNAME")
