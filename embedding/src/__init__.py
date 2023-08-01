af980fb44b16bb7b7aeedaf2cedf0a456d9a002a
Paul Schwind <git@pschwind.de>
2023-08-01 18:45:24 +0200

    Start embedding service faster by including installation step in build

--- embedding/src/__init__.py
+++ embedding/src/__init__.py
@@ -1,7 +1,10 @@
 import spacy
-# Download the model so that importing it will work
-# see https://stackoverflow.com/a/47297686/4306257
-spacy.cli.download("en_core_web_sm")
+try:
+    from spacy.en import English
+except ImportError:
+    # Download the model so that importing it will work
+    # see https://stackoverflow.com/a/47297686/4306257
+    spacy.cli.download("en_core_web_sm")
 
 # Patch import
 from .patch.patch_spacy_tags import TAG_MAP
